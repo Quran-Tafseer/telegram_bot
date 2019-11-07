a7cfe5e9bef2355d7745cacef868bbe099f620c2
Emad Mokhtar <emad.m.habib@gmail.com>
2019-11-07 17:27:50 +0100

    Add setuo,py and update the .gitignore file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='telegram_bot',
+    version='0.1.0',
+    packages=['quran_tafseer_bot'],
+    url='',
+    license='',
+    author='emadmokhtar',
+    author_email='emad.m.habib@gmail.com',
+    description='Quran Tafseer Telegram bot for its APIs'
+)
