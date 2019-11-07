d1bee425ce390e9df7eb0f63e2a058141fdcd33d
Emad Mokhtar <emad.m.habib@gmail.com>
2019-11-07 17:26:39 +0100

    Fix calling user.save() in finally

--- quran_tafseer_bot/commands.py
+++ quran_tafseer_bot/commands.py
@@ -17,6 +17,7 @@ def start(bot, update):
     message = render_message('start.html')
     bot.send_message(chat_id=update.message.chat_id, text=message)
 
+
 @send_action(ChatAction.TYPING)
 def book_list(bot, update):
     """Gets the list of availabe books command.
@@ -26,7 +27,9 @@ def book_list(bot, update):
     """
     books = QuranTafseer.get_tafseer_books()
     message = render_message('book_list.html', books=books)
-    bot.send_message(chat_id=update.message.chat_id, text=message, parse_mode='HTML')
+    bot.send_message(chat_id=update.message.chat_id, text=message,
+                     parse_mode='HTML')
+
 
 @send_action(ChatAction.TYPING)
 def set_book(bot, update, args):
@@ -46,24 +49,25 @@ def set_book(bot, update, args):
         return None
 
     from_user = update.message.from_user
+    user = UserPreference
     try:
         user = UserPreference.create(user_id=from_user['id'],
-        first_name=from_user['first_name'],
-        last_name=from_user['last_name'],
-        username=from_user['username'],
-        book_id=int(book_id)
-        )
+                                     first_name=from_user['first_name'],
+                                     last_name=from_user['last_name'],
+                                     username=from_user['username'],
+                                     book_id=int(book_id)
+                                     )
     except peewee.IntegrityError:
         user = UserPreference.get(UserPreference.user_id == from_user['id'])
-        user.first_name=from_user['first_name']
-        user.last_name=from_user['last_name']
-        user.username=from_user['username']
-        user.book_id=int(book_id)
-    finally:
+        user.first_name = from_user['first_name']
+        user.last_name = from_user['last_name']
+        user.username = from_user['username']
+        user.book_id = int(book_id)
         user.save()
-    #TODO: Send the name of the book in the message instead of the ID only
+    # TODO: Send the name of the book in the message instead of the ID only
     message = render_message('set_book.html', book_id=book_id)
     bot.send_message(chat_id=update.message.chat_id, text=message)
+
 
 @send_action(ChatAction.TYPING)
 def tafseer(bot, update, args):
@@ -81,9 +85,11 @@ def tafseer(bot, update, args):
         bot.send_message(chat_id=update.message.chat_id,
                          text=render_message('tafseer_invalid.html'))
         return None
-    user= UserPreference.get(UserPreference.user_id == update.message.from_user['id'])
+    user = UserPreference.get(
+        UserPreference.user_id == update.message.from_user['id'])
     tafseer_book = QuranTafseer(book_id=user.book_id)
-    tafseer_text = tafseer_book.get_verse_tafseer(chapter_number=chapter_number,
-                                                  verse_number=verse_number)
-    message =  render_message('tafseer.html', tafseer=tafseer_text['text'])
+    tafseer_text = tafseer_book.get_verse_tafseer(
+        chapter_number=chapter_number,
+        verse_number=verse_number)
+    message = render_message('tafseer.html', tafseer=tafseer_text['text'])
     bot.send_message(chat_id=update.message.chat_id, text=message)
